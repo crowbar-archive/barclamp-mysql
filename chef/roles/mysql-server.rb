dea9c680a3854d10deb783ca49e562a4e31b5848
Keith Hudgins <greenman@greenman.org>
2011-09-21 18:29:01 -0400

    Crowbar config munging

--- chef/roles/mysql-server.rb
+++ chef/roles/mysql-server.rb
@@ -1,8 +1,7 @@
 name "mysql-server"
 description "Mysql Server Role"
 run_list(
-         "recipe[mysql::server]",
-         "recipe[mysql::monitor]"
+         "recipe[mysql::server]"
 )
 default_attributes()
 override_attributes()
