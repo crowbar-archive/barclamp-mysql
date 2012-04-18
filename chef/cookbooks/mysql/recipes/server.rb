a05cf916c448f7864a2d1951c8faa7e81a8a200a
cdearborn <Christopher_Dearborn@dell.com>
2012-04-18 13:56:09 -0400

    Fix for defect: DE718: grants.sql is left behind

--- chef/cookbooks/mysql/recipes/server.rb
+++ chef/cookbooks/mysql/recipes/server.rb
@@ -168,21 +168,20 @@ grants_path = value_for_platform(
   "default" => "/etc/mysql/grants.sql"
 )
 
-begin
-  t = resources("template[#{grants_path}]")
-rescue
-  Chef::Log.info("Could not find previously defined grants.sql resource")
-  t = template grants_path do
-    source "grants.sql.erb"
-    owner "root"
-    group "root"
-    mode "0600"
-    action :create
-  end
+template grants_path do
+  source "grants.sql.erb"
+  owner "root"
+  group "root"
+  mode "0600"
+  action :create
 end
 
 execute "mysql-install-privileges" do
   command "/usr/bin/mysql -u root #{node['mysql']['server_root_password'].empty? ? '' : '-p' }#{node['mysql']['server_root_password']} < #{grants_path}"
   action :nothing
   subscribes :run, resources("template[#{grants_path}]"), :immediately
+end
+
+file "#{grants_path}" do
+  action :delete
 end
